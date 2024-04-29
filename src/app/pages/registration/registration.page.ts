4b2de96eb797097eff97548602125ce4c3b1005f
J48PC18 <J48PC18@uslt.lab>
2024-04-29 17:54:06 +0800

    updated registration

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
@@ -7,15 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 fname=""
-lname=""
-mname=""
-cellnum=""
+username=""
 email=""
 pass=""
 conpass=""
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
+validation(){
+let message =""
+  if(this.fname =="")
+  message += "Fullname is required\n"
+  if(this.username =="")
+  message += "Username is required\n"
+  if(this.email =="")
+  message += "Email is required\n"
+  if(this.pass =="")
+  message += "Password is required\n"
+
+  if(this.pass != this.conpass)
+  message += "Password does not match\n"
+
+  if(message=="")
+  return true
+   
+  alert(message)
+  return false;
+  
+}
+
+
+
+addDAta(){
+  let formData = new FormData();
+  formData.append('username',this.username)
+  formData.append('fullname',this.fname)
+  formData.append('email',this.email)
+  formData.append('password',this.pass)
+  let url='https://webtechnologies.site/714/Bayle/api/registration.php'
+  this.http.post<any>(url,formData).subscribe(
+    (data:any)=>{
+      console.log(data)
+    }
+  )
+}
+
+register(){
+  if (this.validation()){
+    this.addDAta()
+  }
+}
 
 }
